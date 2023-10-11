59897cdffa2825364a3b239870d7def48cf8b5cb
Dimas Ernanda <dimasernanda@students.amikom.ac.id>
2023-10-11 17:03:19 +0700

    update

---
+++ app/src/main/java/BukuModel.kt
@@ -0,0 +1,4 @@
+import android.widget.ImageView
+
+class BukuModel(var image:Int, var title:String,var desc:String) {
+}
--- app/src/main/java/com/example/projectbp2710/HomeActivity2.kt
+++ app/src/main/java/com/example/projectbp2710/HomeActivity2.kt
@@ -1,11 +1,34 @@
 package com.example.projectbp2710
 
+import AdapterHome
+import BukuModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home2)
+
+        val btnMore:Button = findViewById(R.id.ButtonMenu4)
+        val btnKid:Button = findViewById(R.id.ButtonMenu1)
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+
+        rvBuku.layoutManager = LinearLayoutManager (this)
+
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1, "Emi's Beach Adventure",
+            "lorem jasgjagdahh jasdghjagahj uhaskj"))
+        data.add(BukuModel(R.drawable.book2, "Ade's Adventure",
+            "lorem jasgjagdahh jasdghjagahj uhaskj"))
+        data.add(BukuModel(R.drawable.book4, "Mermaid To Rescue",
+            "lorem jasgjagdahh jasdghjagahj uhaskj"))
+
+        val adapter = AdapterHome(data)
+
+        rvBuku.adapter = adapter
     }
 }
